a1321b205e7bb62f6df0e52aa5e5a8adc3e71c62
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-02-29 14:13:58 -0300

    key

--- secao03/src/components/ListRender.jsx
+++ secao03/src/components/ListRender.jsx
@@ -2,11 +2,24 @@ import { useState } from 'react';
 
 const ListRender = () => {
   const [list] = useState(['Camilla', 'Gui', 'Cauã']);
+  const [users] = useState([
+    { id: 1, nome: 'matheus', idade: 28 },
+    { id: 482518, nome: 'felipe', idade: 24 },
+    { id: 12526, nome: 'galileu', idade: 52 },
+  ]);
+
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.nome} - {user.idade}
+          </li>
         ))}
       </ul>
     </div>
