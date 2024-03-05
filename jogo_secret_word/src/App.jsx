38a700e324287a1eec6163be5e39285cb676aa1f
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-05 16:37:16 -0300

    create

---
+++ jogo_secret_word/src/App.jsx
@@ -0,0 +1,29 @@
+//css
+import './App.css';
+//react
+import { useCallback, useEffect, useState } from 'react';
+//dados
+import { wordsList } from './data/word';
+//components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+];
+function App() {
+  const [gameStage, setGameStage] = useState(stages(0).name);
+  const [words] = useState(wordsList);
+  return (
+    <div className="App">
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
---
+++ jogo_secret_word/src/data/word.js
@@ -0,0 +1,10 @@
+export const wordsList = {
+  carro: ['Motor', 'Porta', 'Rodas', 'Faróis', 'Volante'],
+  fruta: ['banana', 'maçã', 'uva', 'morango', 'abacaxi'],
+  animal: ['leão', 'elefante', 'girafa', 'tigre', 'macaco'],
+  cor: ['vermelho', 'azul', 'verde', 'amarelo', 'roxo'],
+  instrumento: ['violino', 'piano', 'flauta', 'guitarra', 'bateria'],
+  cidade: ['Paris', 'Nova York', 'Tóquio', 'Londres', 'Roma'],
+  esporte: ['futebol', 'tênis', 'natação', 'basquete', 'atletismo'],
+  profissao: ['médico', 'engenheiro', 'professor', 'advogado', 'cozinheiro'],
+};
