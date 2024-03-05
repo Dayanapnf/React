0ddda49057ee989801ad85e3160318acc3fd37bf
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-05 18:14:05 -0300

    navegacao

--- jogo_secret_word/src/App.jsx
+++ jogo_secret_word/src/App.jsx
@@ -15,13 +15,25 @@ const stages = [
   { id: 3, name: 'end' },
 ];
 function App() {
-  const [gameStage, setGameStage] = useState(stages(0).name);
+  const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+
+  //starts
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
