ff273768e9524362855fe8889399387d90250962
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-12 09:25:10 -0300

    api local

--- req_http/src/App.jsx
+++ req_http/src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+const url = 'http://localhost:3000/products';
 function App() {
+  const [produtos, setProdutos] = useState([]);
+
+  //1. resgatando os dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProdutos(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul style={{ listStyle: 'none' }}>
+        {produtos.map((produtos) => (
+          <li key={produtos.id}>
+            {produtos.name} - R$: {produtos.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
