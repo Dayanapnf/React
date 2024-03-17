a0764f07b9a1172d618354b68892288a5cb9fdd7
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-16 21:30:00 -0300

    up

--- secao04/src/App.jsx
+++ secao04/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
 import './App.css';
 
-//1- config react route
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// 1 - config react router, sem links
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+// pages
 import Home from './pages/Home';
 import About from './pages/About';
-import Products from './pages/Products';
+
+// 2 - adicionando links
+// components
 import Navbar from './components/Navbar';
+import Products from './pages/Products';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import Search from './pages/Search';
+import { SearchForm } from './components/SearchForm';
 
 function App() {
   return (
@@ -17,12 +22,21 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        {/* 9 - search */}
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          //rota dinamica
-          <Route path="/products/:id" element={<Products />} />
+          <Route path="about" element={<About />} />
+          {/* 6 - nested route */}
           <Route path="/products/:id/info" element={<Info />} />
+          {/* 4 - rota dinamica */}
+          <Route path="/products/:id" element={<Product />} />
+
+          {/* 9 search params */}
+          <Route path="/search" element={<Search />} />
+          {/* 10 - redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
+          {/* 7  - no match route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- secao04/src/components/Navbar.jsx
+++ secao04/src/components/Navbar.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 import './Navbar.css';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="./">Home</Link>
-      <Link to="./about">About</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+      {/* 8 - active link */}
+      <NavLink
+        className={({ isActive }) => (isActive ? 'active' : undefined)}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? 'active' : undefined)}
+        to="/about"
+      >
+        Sobre
+      </NavLink>
     </nav>
   );
 };
