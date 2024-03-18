bef61f80c235873c09eb1b76f066e90faf85e53f
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-18 09:08:43 -0300

    context

--- secao05/src/pages/Home.jsx
+++ secao05/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CounterContext } from '../context/CounterContext';
+import ChangeCounter from './ChangeCounter';
 
 const Home = () => {
+  const { counter } = useContext(CounterContext);
   return (
     <div>
       <h1>Home</h1>
+      <h3>Valor do contador:{counter}</h3>
+      <ChangeCounter />
     </div>
   );
 };
