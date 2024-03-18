72d950655046d15efd2ec80abfc909e6a0451732
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-18 09:21:15 -0300

    context about

--- secao05/src/pages/About.jsx
+++ secao05/src/pages/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useCounterContext } from '../hooks/useCountercontext';
 
 const About = () => {
+  const { counter } = useCounterContext();
   return (
     <div>
       <h1>About</h1>
+      <h3>Valor do contador é: {counter}</h3>
     </div>
   );
 };
