b73c71e6ce306d6f026936fc356e299717402fa2
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-18 09:19:43 -0300

    refatorando

--- secao05/src/pages/Home.jsx
+++ secao05/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react';
 import { CounterContext } from '../context/CounterContext';
 import ChangeCounter from './ChangeCounter';
+import { useCounterContext } from '../hooks/useCountercontext';
 
 const Home = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+
+  const { counter } = useCounterContext();
   return (
     <div>
       <h1>Home</h1>
